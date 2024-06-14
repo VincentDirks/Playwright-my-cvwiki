c21972b8a7828421427a42e5682acd94347c6b09
VincentDirks <vincent@dirksonline.net>
2024-06-14 20:54:12 +1200

    tidy up

--- tests/cvwiki.spec.ts
+++ tests/cvwiki.spec.ts
@@ -3,7 +3,4 @@ import { test, expect } from "@playwright/test"
 test("Landing Page - Screenshot", async ({ page }) => {
   await page.goto("")
   await expect(page.locator("body")).toHaveScreenshot()
-
-  // Expect a title "to contain" a substring.
-  //  await expect(page).toHaveTitle(/Curriculum Vitae - Vincent Dirks - Vincents CV Wiki/);
 })
